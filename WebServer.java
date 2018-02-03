efbc205fab1b9175568e71758c097963703d97e1
Ben Douglas <dougers437@gmail.com>
2018-02-03 13:01:57 -0500

    main method

--- WebServer.java
+++ WebServer.java
@@ -1,4 +1,3 @@
-package Assignment_One;
 import java.io.* ;
 import java.net.* ;
 import java.util.* ;
@@ -6,37 +5,53 @@ import java.util.* ;
 public class WebServer implements Runnable{
 	ArrayList <Book> bib = null;
 	private Thread currentThread = null;
-	final int port;
+	private int port;
 	ServerSocket serverSocket = null;
 	public boolean isRunning = true;
-	public WebServer(int port) {
-		this.port=port;
-	}
-    public void run() {
-    	
-    this.currentThread = Thread.currentThread();
-	try {
-		this.serverSocket =  new ServerSocket(port);
-	}	catch(IOException e) {
-		System.err.format("Could not listen on port : %d",port);
-		System.exit(1);
+	
+	public static void main(String[] args) {
+		new WebServer(5555);
 	}
 	
-	while (isRunning) {
-		Socket clientSocket = null;
-		try {
-	        clientSocket = serverSocket.accept();
-	        new Thread(new CsapProtocol(clientSocket)).start();;
-	    } catch (IOException e) {
-	        System.err.println("Accept failed.");
-	        System.exit(1);
-	    }
+//	
+	public WebServer(int port) {
+		this.port=port;
 		
-	    // Start the thread.
+		this.run();
 	}
+
+	
+	
+    public void run() {
+    	
+    	System.out.print("Yoo");
+    	
+	    this.currentThread = Thread.currentThread();
+		try {
+			this.serverSocket =  new ServerSocket(port);
+			
+		}	catch(IOException e) {
+			System.err.format("Could not listen on port : %d",port);
+			System.exit(1);
+		}
+		
+		while (isRunning) {
+			Socket clientSocket = null;
+			try {
+		        clientSocket = serverSocket.accept();
+		        new Thread(new CsapProtocol(clientSocket)).start();
+		        
+		        System.out.print("Yoooo");
+		        
+		    } catch (IOException e) {
+		        System.err.println("Accept failed.");
+		        System.exit(1);
+		    }
+			
+		    // Start the thread.
+		}
     }
  
     
     
 }
-
